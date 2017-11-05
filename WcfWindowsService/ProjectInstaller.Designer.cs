56d5d1f5c1c4b88527d946d0d241f3bb948e65a7
mikeythongphan <thong_pdt@hotmail.com>
2017-11-05 13:20:06 +0700

    Add files via upload

---
+++ WcfWindowsService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WcfWindowsService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wcfServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.wcfServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // wcfServiceProcessInstaller
+            // 
+            this.wcfServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.NetworkService;
+            this.wcfServiceProcessInstaller.Password = null;
+            this.wcfServiceProcessInstaller.Username = null;
+            // 
+            // wcfServiceInstaller
+            // 
+            this.wcfServiceInstaller.Description = "Wcf Simpay Service";
+            this.wcfServiceInstaller.DisplayName = "Wcf Simpay Service";
+            this.wcfServiceInstaller.ServiceName = "WcfService";
+            this.wcfServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.wcfServiceProcessInstaller,
+            this.wcfServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller wcfServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller wcfServiceInstaller;
+    }
+}
---
+++ WcfWindowsService/WcfService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+using 
+
+namespace WcfWindowsService
+{
+    public partial class WcfService : ServiceBase
+    {
+        public WcfService()
+        {
+            InitializeComponent();
+        }
+
+        internal static ServiceHost wcfServiceHost = null; 
+
+        protected override void OnStart(string[] args)
+        {
+            if (wcfServiceHost != null)
+            {
+                wcfServiceHost.Close();
+            }
+            wcfServiceHost = new ServiceHost(typeof(WcfService));
+            wcfServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (wcfServiceHost != null)
+            {
+                wcfServiceHost.Close();
+                wcfServiceHost = null;
+            }
+        }
+    }
+}
